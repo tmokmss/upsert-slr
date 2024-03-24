c9325e02725ee0a71f1595489724fb64f08cc883
tmokmss <tmokmss@users.noreply.github.com>
2024-03-24 13:50:29 +0900

    fix: handle resource physical ID properly

--- lambda/index.ts
+++ lambda/index.ts
@@ -19,38 +19,43 @@ export const handler: Handler<Event> = async (event, context) => {
   try {
     switch (event.RequestType) {
       case 'Create':
-        console.log(`trying to create a service linked role for ${event.ResourceProperties.awsServiceName}`);
-        const command = new CreateServiceLinkedRoleCommand({
-          AWSServiceName: event.ResourceProperties.awsServiceName,
-          Description: event.ResourceProperties.description,
-          CustomSuffix: event.ResourceProperties.customSuffix,
-        });
-        await client.send(command);
-        console.log('the service linked role created successfully, now waiting for IAM propagation');
-        await setTimeout(event.ResourceProperties.waitTimeSeconds * 1000);
-        break;
       case 'Update':
+        console.log(`trying to create a service linked role for ${event.ResourceProperties.awsServiceName}`);
+        try {
+          const command = new CreateServiceLinkedRoleCommand({
+            AWSServiceName: event.ResourceProperties.awsServiceName,
+            Description: event.ResourceProperties.description,
+            CustomSuffix: event.ResourceProperties.customSuffix,
+          });
+          await client.send(command);
+          console.log('the service linked role created successfully, now waiting for IAM propagation');
+          await setTimeout(event.ResourceProperties.waitTimeSeconds * 1000);
+        } catch (e) {
+          if (e instanceof InvalidInputException) {
+            console.log(`The service linked role seems to already exist, skipping the creation... ${e.message}`);
+            break;
+          } else {
+            throw e;
+          }
+        }
         break;
       case 'Delete':
         break;
     }
     await sendStatus('SUCCESS', event, context);
   } catch (e) {
-    if (e instanceof InvalidInputException) {
-      console.log(`The service linked role seems to already exist, skipping the creation... ${e.message}`);
-      await sendStatus('SUCCESS', event, context);
-    } else {
-      const err = e as Error;
-      await sendStatus('FAILED', event, context, err.message);
-    }
+    const err = e as Error;
+    await sendStatus('FAILED', event, context, err.message);
   }
 };
 
 const sendStatus = async (status: 'SUCCESS' | 'FAILED', event: Event, context: any, reason?: string) => {
+  const props = event.ResourceProperties;
+  const physicalId = `${props.awsServiceName}-${props.customSuffix ?? ''}`;
   const responseBody = JSON.stringify({
     Status: status,
-    Reason: reason ?? 'See the details in CloudWatch Log Stream: ' + context.logStreamName,
-    PhysicalResourceId: context.logStreamName,
+    Reason: (reason ?? '') + ' See the details in CloudWatch Log Stream: ' + context.logStreamName,
+    PhysicalResourceId: physicalId,
     StackId: event.StackId,
     RequestId: event.RequestId,
     LogicalResourceId: event.LogicalResourceId,
